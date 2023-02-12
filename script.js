6761c19b3bea8b8ad8d2ee3643c64dbfa8c582a3
Real <RealSkaarj@gmx.de>
2023-02-12 15:22:11 +0100

    stats etc

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ let pokeImage;
 let evolutionChain;
 
 function onLoad() {
-  setCount()
+  // setCount()
   iteratePokemonList()
 }
 
-async function setCount() {
-  let url = `https://pokeapi.co/api/v2/pokemon/`;
-  let response = await fetch(url);
-  pokemonList = await response.json();
-  count = pokemonList['count'];
-  console.log(count);
-}
+// async function setCount() {
+//   let url = `https://pokeapi.co/api/v2/pokemon/`;
+//   let response = await fetch(url);
+//   pokemonList = await response.json();
+//   count = pokemonList['count'];
+//   console.log(count);
+// }
 
 async function iteratePokemonList() {
-  let url = `https://pokeapi.co/api/v2/pokemon/?offset=1&limit=20/`;
+  let url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20/`;
   let response = await fetch(url);
   pokemonList = await response.json();
   for (let i = 0; i < 20; i++) {
@@ -54,7 +54,7 @@ function renderPokemon(i) {
   let PokeDex = document.getElementById('pokedex');
   PokeDex.innerHTML +=/*html*/ `
       <div onclick="" id="pokeMon${i}" class="pokeMon">
-          <h2 class="pokemonName"><img class="poke-ball" src="img/pokemon.gif" alt="">${PokeName}</h2>
+          <h2 class="pokemonName"><img class="poke-ball" src="img/animatedpokeball.gif" alt="">${PokeName}</h2>
           <div id="elements${i}" class="elements">
           </div>
           <img class="poke-ball-background" src="img/Pokeballbackground.svg" alt="">
@@ -99,10 +99,8 @@ function insertInfoContainer(i) {
             <div class="tab-pane about-tab tab-pane-${i} fade show active" id="about-tab-pane${i}" role="tabpanel" tabindex="0">
             </div>
             <div class="tab-pane tab-pane-${i} fade" id="stats-tab-pane${i}" role="tabpanel" tabindex="0">
-            Stats
             </div>
             <div class="tab-pane evo-tab tab-pane-${i} fade" id="evolution-tab-pane${i}" role="tabpanel" tabindex="0">
-            Evolution in here
             </div>
             <div class="tab-pane move-tab tab-pane-${i} fade" id="moves-tab-pane${i}" role="tabpanel" tabindex="0">
             </div>
@@ -124,46 +122,64 @@ function activateTab(i) {
   document.querySelector(clickedTab.dataset.bsTarget).classList.add('show', 'active');
 }
 
+/* -------------------------animation test ----------------*/
+
+// function animateBars(i){
+//   let statsTab = document.getElementById('statsTab'+ i);
+//   let statBars = document.querySelectorAll('.progress-bar');
+//   if (statsTab.classList.contains('active')) {
+//     statBars.forEach(function(statBar){
+//       statBar.style.cssText = 'transform: scaleX(3)!important; transition: 300ms all ease-in-out';
+//     });
+//   } else {
+//     statBars.forEach(function(statBar){
+//       statBar.style.cssText = 'transform: scaleX(3)!important; transition: 300ms all ease-in-out';
+//     });
+//   }
+// }
+
 /* -------------- chart ------------- */
+
 
 function insertStats(i) {
   let statsTab = document.getElementById('stats-tab-pane' + i);
   statsTab.innerHTML =/*html*/ `
   <div class="chart-box">
-      <div class="progress" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-hp" style="width: 25%">HP</div>
+      <div class="progress" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-hp" style="width: ${pokemonData.stats[0].base_stat}%">${pokemonData.stats[0].base_stat} - HP</div>
       </div>
-      <div class="progress" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-attack" style="width: 50%">ATK</div>
+      <div class="progress" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-attack" style="width: ${pokemonData.stats[1].base_stat}%;">${pokemonData.stats[1].base_stat} - ATK</div>
       </div>
-      <div class="progress" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-defense" style="width: 75%">DEF</div>
+      <div class="progress" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-defense" style="width: ${pokemonData.stats[2].base_stat}%;">${pokemonData.stats[2].base_stat} - DEF</div>
       </div>
-      <div class="progress" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-special-attack" style="width: 100%; padding-bottom: 1px;">S-ATK</div>
+      <div class="progress" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-special-attack" style="width: ${pokemonData.stats[3].base_stat}%; padding-bottom: 1px;">${pokemonData.stats[3].base_stat} - S-ATK</div>
       </div> 
-      <div class="progress" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-special-defense" style="width: 100%; padding-bottom: 0px;">S-DEF</div>
+      <div class="progress" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-special-defense" style="width: ${pokemonData.stats[4].base_stat}%; padding-bottom: 0px;">${pokemonData.stats[4].base_stat} - S-DEF</div>
       </div> 
-      <div class="progress" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-speed" style="width: 100%">SPD</div>
+      <div class="progress" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100">
+          <div class="progress-bar bg-speed" style="width: ${pokemonData.stats[5].base_stat}%;">${pokemonData.stats[5].base_stat} - SPD</div>
       </div> 
   </div>
   `;
 }
 
-function insertAbout(i){
+
+function insertAbout(i) {
   let about = pokemonSpecies.flavor_text_entries[1].flavor_text;
-  let aboutField = document.getElementById('about-tab-pane'+ i);
+  let aboutField = document.getElementById('about-tab-pane' + i);
   aboutField.innerHTML = `
   <span class="about">${about.replace(/\f/g, ' ')} </span>
   `;
 }
 
-function insertMoves(i){
-let moveTab = document.getElementById('moves-tab-pane' + i);
+function insertMoves(i) {
+  let moveTab = document.getElementById('moves-tab-pane' + i);
 
- for (let j = 0; j < pokemonData.moves.length; j++) {
+  for (let j = 0; j < pokemonData.moves.length; j++) {
     const move = pokemonData.moves[j].move.name;
     moveTab.innerHTML +=/*html*/ `
     <span class="move">${move}</span>
@@ -171,21 +187,43 @@ let moveTab = document.getElementById('moves-tab-pane' + i);
   }
 }
 
-  async function insertEvolution(i){
+async function insertEvolution(i) {
   let url = pokemonSpecies.evolution_chain.url;
   let evo1 = await loadEvolutionChain(url);
-  evo1 = await evolutionChain.chain.species.name
+  evo1 = await evolutionChain.chain.species.name;
+  evo2 = await evolutionChain.chain.evolves_to[0].species.name;
+  evo3 = await evolutionChain.chain.evolves_to[0].evolves_to[0].species.name;
 
- 
   let evolution = document.getElementById('evolution-tab-pane' + i);
   evolution.innerHTML +=/*html*/ `
-    <img class="evo-image" src="${await loadPokemonEvolution(evo1)}" alt=""><img class="arrow-image" src="img/arrowright.png" alt="">
-    <img class="evo-image" src="" alt=""><img class="arrow-image" src="img/arrowright.png" alt="">
-    <img class="evo-image" src="" alt="">
-   `;
- }
+   <h5 class="evo-name">
+      <img id="evoImage1-${i}" class="evo-image" src="${await loadPokemonEvolution(evo1)}" alt="">
+      ${evo1}
+   </h5>
+   <img class="arrow-image" src="img/arrowright.png" alt="">
+   <h5 class="evo-name">   
+      <img id="evoImage2-${i}" class="evo-image" src="${await loadPokemonEvolution(evo2)}" alt="">
+      ${evo2}
+   </h5>
+   <img class="arrow-image" src="img/arrowright.png" alt="">
+   <h5 class="evo-name">
+      <img id="evoImage3-${i}" class="evo-image" src="${await loadPokemonEvolution(evo3)}" alt="">
+      ${evo3}
+   </h5>
+  `;
+  {
+    if (evo1 == currentPokemon) {
+      document.getElementById('evoImage1-' + i).classList.add('glow')
+    } else if (evo2 == currentPokemon) {
+      document.getElementById('evoImage2-' + i).classList.add('glow')
+    } else if (evo3 == currentPokemon){
+      document.getElementById('evoImage3-' + i).classList.add('glow')
+    }
 
- async function loadPokemonEvolution(evo) {
+  }
+}
+
+async function loadPokemonEvolution(evo) {
   let url = `https://pokeapi.co/api/v2/pokemon/${evo}`;
   let response = await fetch(url);
   evo = await response.json();
